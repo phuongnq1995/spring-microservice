4400dd0857cdb18300171936518f3b1584426c8b
phuongnq1995 <phuongnq1995@github.com>
2018-03-27 08:30:43 +0700

    add

--- auth-server/src/main/java/pl/piomin/microvercies/auth/config/Application.java
+++ auth-server/src/main/java/pl/piomin/microvercies/auth/config/Application.java
@@ -1,15 +1,25 @@
 package pl.piomin.microvercies.auth.config;
 
+import java.security.Principal;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@RestController
 public class Application {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+	
+	@RequestMapping("/user")
+	public Principal info(Principal user) {
+		return user;
 	}
 	
 }
---
+++ customer-service/src/main/java/pl/piomin/microservices/customer/ResourceServerConfig.java
@@ -0,0 +1,24 @@
+/*package pl.piomin.microservices.customer;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+@Configuration
+@EnableResourceServer
+@EnableWebSecurity
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
+	
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+				.anyRequest().authenticated().and()
+			.sessionManagement()
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			.and().csrf().disable();
+	}
+}
+*/
